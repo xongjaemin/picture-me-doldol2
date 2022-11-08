daa440bffc8d29bfdacc0e331b19bcb60df329e8
masibasi <jimin_lee98@naver.com>
2022-11-08 20:37:38 +0900

    add jaemin gesture

--- gestures/tello_gesture_controller.py
+++ gestures/tello_gesture_controller.py
@@ -16,12 +16,21 @@ class TelloGestureController:
         gesture_id = gesture_buffer.get_gesture()
         print("GESTURE", gesture_id)
 
+        # if gesture_id == 8: # ROCK
+        #     print("rock")
+        #     #self.tello.takeoff()
+        #     self.tello.flip_right()
+
         if not self._is_landing:
             if gesture_id == 0:  # Forward
                 self.forw_back_velocity = 30
             elif gesture_id == 1:  # STOP
+                self._is_landing = True
                 self.forw_back_velocity = self.up_down_velocity = \
                     self.left_right_velocity = self.yaw_velocity = 0
+                self.tello.land()
+            elif gesture_id == 8: # ROCK
+                self.tello.flip_right()
             if gesture_id == 5:  # Back
                 self.forw_back_velocity = -30
 
@@ -30,11 +39,11 @@ class TelloGestureController:
             elif gesture_id == 4:  # DOWN
                 self.up_down_velocity = -25
 
-            elif gesture_id == 3:  # LAND
-                self._is_landing = True
-                self.forw_back_velocity = self.up_down_velocity = \
-                    self.left_right_velocity = self.yaw_velocity = 0
-                self.tello.land()
+            # elif gesture_id == 3:  # LAND
+            #     self._is_landing = True
+            #     self.forw_back_velocity = self.up_down_velocity = \
+            #         self.left_right_velocity = self.yaw_velocity = 0
+            #     self.tello.land()
 
             elif gesture_id == 6: # LEFT
                 self.left_right_velocity = 20
@@ -47,3 +56,5 @@ class TelloGestureController:
 
             self.tello.send_rc_control(self.left_right_velocity, self.forw_back_velocity,
                                        self.up_down_velocity, self.yaw_velocity)
+
+            
